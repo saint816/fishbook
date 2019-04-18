a43739ba010a751e1a23ea9146fe08bf8d5fb36f
pengsheng <>
2019-04-18 10:59:48 +0800

    配置Pycharm虚拟环境变量

---
+++ fisher.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     fisher
+   Description :
+   Author :       pengsheng
+   date：          2019-04-18
+-------------------------------------------------
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+app.run()
